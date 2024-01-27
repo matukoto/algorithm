a9cb9ebbe595df21f130a72962207a9519ff5f62
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-01-27 12:22:34 +0900

    feat: bard さんに教えてもらった修正版

--- lesson1.java
+++ lesson1.java
@@ -11,21 +11,19 @@ public class lesson1 {
       inputList.add(num);
     }
 
-    String joinList1 = inputList.stream().map(String::valueOf).collect(Collectors.joining(" "));
-    System.out.println(joinList1);
+    System.out.println(inputList.stream().map(String::valueOf).collect(Collectors.joining(" ")));
     for (int i = 1; i < N; i++) {
       int j = i - 1;
       int targetNum = inputList.get(i);
-      while (inputList.get(j + 1) < inputList.get(j)) {
+
+      while (j >= 0 && inputList.get(j) > targetNum) {
         inputList.set(j + 1, inputList.get(j));
-        inputList.set(j, targetNum);
-        if (j > 0) {
-          j--;
-        }
+        j--;
       }
 
-      String joinList = inputList.stream().map(String::valueOf).collect(Collectors.joining(" "));
-      System.out.println(joinList);
+      inputList.set(j + 1, targetNum);
+      System.out
+          .println(inputList.stream().map(String::valueOf).collect(Collectors.joining(" ")));
     }
 
     sc.close();
