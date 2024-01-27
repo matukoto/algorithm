565460122aec17052dfd5e3a94c4599f5d553678
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-01-27 13:35:55 +0900

    feat: いったんできた。でもaojだとTLEになる。なんでだろう。あと、最大公約数の候補リストを作成するところがダサいので、もっといい方法があるはず。
    [optional body]
    ↑ copilot の補完ででてきたコメント(「いったんできた。でもaoj」まで打ってでてきた)。バカにされている。

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.*;
 
 /**
  * 最大公約数を求める
@@ -13,17 +12,31 @@ public class GreatestCommonDivisor {
     var aList = hoge(a);
     var bList = hoge(b);
 
+    // 最大公約数の候補リストを作成する
+    var commonList = new ArrayList<Integer>();
+    for (int targetA : aList) {
+      for (int targetB : bList) {
+        if (targetA > targetB) {
+          break;
+        }
+        if (targetA == targetB) {
+          commonList.add(targetA);
+          break;
+        }
+      }
+    }
+
+    System.out.println(commonList.stream().max(Comparator.comparingInt(i -> i)).get());
   }
 
-  // 約数のリストを求める
+  // 約数のリストを求める (大きい順)
   private static List<Integer> hoge(int num) {
     var retList = new ArrayList<Integer>();
 
     // num 以下の数字でひたすら余りが0のものを返せばいい
-    for (int i = 1; i <= num; i++) {
+    for (int i = num; i > 0; i--) {
       if (num % i == 0) {
         retList.add(i);
-        System.out.println(i);
       }
     }
     return retList;
