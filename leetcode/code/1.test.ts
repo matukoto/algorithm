c4a6643a5399351b0db46b933a9b0325c750fdad
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-12-11 12:12:18 +0900

    feat(leetcode):  clear 1 two sum test

--- leetcode/code/1.test.ts
+++ leetcode/code/1.test.ts
@@ -1,8 +1,17 @@
 import { describe, it, expect } from "vitest";
 import { twoSum } from "./1.two-sum.ts";
 
-describe("sum function", () => {
-  it("should return 3 when adding 1 and 2", () => {
-    expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
+describe("two-sum", () => {
+  it("シンプル 2つの要素がそのまま答えになる", () => {
+    expect(twoSum([2, 7], 9)).toEqual([0, 1]);
+  });
+  it("例題1 最初の2つの要素が答え", () => {
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+  });
+  it("例題2 最初の2つ以外が答え", () => {
+    expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+  });
+  it("例題3 同じ値でも同じ要素を参照しないチェック", () => {
+    expect(twoSum([3, 3], 6)).toEqual([0, 1]);
   });
 });
--- leetcode/code/1.two-sum.ts
+++ leetcode/code/1.two-sum.ts
@@ -1,7 +1,19 @@
-export function twoSum(nums: number[], target: number): number[] {
-  // for ()
+export //
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    const diff = difference(nums[i], target);
+    // console.log(`diff=${diff}`);
+
+    for (let j = i + 1; j < nums.length; j++) {
+      // console.log(`j=${nums[j]}`);
+      if (nums[j] == diff) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
 }
 
-function difference(num1: number, target: number): number {
+const difference = (num1: number, target: number): number => {
   return target - num1;
-}
+};
