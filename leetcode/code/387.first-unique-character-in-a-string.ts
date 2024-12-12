98995fe713203fbb64eeade7d91d19ccf50201bf
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-12-12 23:42:26 +0900

    feat(leetcode):  387 ok [optional body] Runtime: 17 ms, faster than 93% of TypeScript online submissions for First Unique Character in a String.
    Memory Usage: 59.3 MB, less than 13% of TypeScript First Unique Character in a String.

--- leetcode/code/387.first-unique-character-in-a-string.ts
+++ leetcode/code/387.first-unique-character-in-a-string.ts
@@ -1,3 +1,19 @@
-export function firstUniqChar(s: string): number {
-  return 1;
+function firstUniqChar(s: string): number {
+  const ss = s.split("");
+
+  let tyofukus: Array<string> = [];
+  outer: for (let i = 0; i < ss.length; i++) {
+    if (tyofukus.includes(ss[i])) {
+      continue;
+    }
+    for (let j = i + 1; j < ss.length; j++) {
+      if (ss[i] == ss[j]) {
+        tyofukus.push(ss[i]);
+        continue outer;
+      }
+    }
+    return i;
+  }
+
+  return -1;
 }
--- leetcode/code/387.test.ts
+++ leetcode/code/387.test.ts
@@ -1,2 +1,23 @@
-import { * } from 'vitest'
-import { firstUniqChar } from './387.first-unique-cahacter-in-a-string.ts'
+import { describe, expect, it } from "vitest";
+import {
+  firstUniqChar,
+  // createStringList,
+} from "./387.first-unique-character-in-a-string.ts";
+
+describe("重複していない最初の文字のindexを返す", () => {
+  it("最初の1文字が重複なし", () => {
+    expect(firstUniqChar("leetcode")).toBe(0);
+  });
+  it("3文字目で初めて重複なし", () => {
+    expect(firstUniqChar("loveleetcode")).toBe(2);
+  });
+  it("全て重複していれば-1", () => {
+    expect(firstUniqChar("aabb")).toBe(-1);
+  });
+});
+
+// describe("文字列を分解してリストをつくる", () => {
+//   it("huga", () => {
+//     expect(createStringList("ll")).toEqual(["l", "l"]);
+//   });
+// });
