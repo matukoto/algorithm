1c99ac154e33fa2ae0fe7ccb716eb73ec695ede9
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-12-12 23:49:18 +0900

    fix():  const -> var にしてちょっとだけ改善 [optional body] Runtime: 13 ms, faster than 95% of TypeScript online submissions for First Unique Character in a String.
    Memory Usage: 58.8 MB, less than 16% of TypeScript First Unique Character in a String.

--- leetcode/code/387.first-unique-character-in-a-string.ts
+++ leetcode/code/387.first-unique-character-in-a-string.ts
@@ -1,12 +1,13 @@
 function firstUniqChar(s: string): number {
-  const ss = s.split("");
+  var ss = s.split("");
+  var size = ss.length;
 
   let tyofukus: Array<string> = [];
-  outer: for (let i = 0; i < ss.length; i++) {
+  outer: for (let i = 0; i < size; i++) {
     if (tyofukus.includes(ss[i])) {
       continue;
     }
-    for (let j = i + 1; j < ss.length; j++) {
+    for (let j = i + 1; j < size; j++) {
       if (ss[i] == ss[j]) {
         tyofukus.push(ss[i]);
         continue outer;
