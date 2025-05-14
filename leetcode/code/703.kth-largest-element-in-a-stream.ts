3a673d4a72b50891da5ba9b41441afb71fe0ddc1
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2025-05-14 23:46:48 +0900

    feat(703): とりあえず動く
    [optional body]
    timeout するので sort を毎回するのを辞める必要あり

--- leetcode/code/703.kth-largest-element-in-a-stream.ts
+++ leetcode/code/703.kth-largest-element-in-a-stream.ts
@@ -1,11 +1,21 @@
-export class KthLargest {
-  constructor(k: number, nums: number[]) {}
-
-  add(val: number): number {}
-}
-
 /**
  * Your KthLargest object will be instantiated and called as such:
  * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
+ * 最初の数字の順位の番号を返す問題。
+ * どんどん追加されていくのでそれを都度ソートする
  */
+export class KthLargest {
+	private k: number;
+	private sortedNums: number[];
+	constructor(k: number, nums: number[]) {
+		this.k = k;
+		this.sortedNums = nums;
+	}
+
+	add(val: number): number {
+		this.sortedNums.push(val);
+		this.sortedNums.sort((a, b) => b - a);
+		return this.sortedNums[this.k - 1];
+	}
+}
