1063a4b45cf03a075885b9ddd6b7b8d5e28ed68d
MATSUMOTO Keisuke <matukotoo@gmail.com>
2026-05-07 15:47:26 +0900

    703 java - KthLargestクラスとそのテストクラスを追加

---
+++ leetcode/code/KthLargest.java
@@ -0,0 +1,15 @@
+// 703. Kth Largest
+//
+/**
+ * Your KthLargest object will be instantiated and called as such: var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val) 最初の数字の順位の番号を返す問題。 どんどん追加されていくのでそれを都度ソートする
+ */
+class KthLargest {
+
+
+  KthLargest(int k, int[] nums) {}
+
+  public int add(int val) {
+    return 0;
+  }
+}
