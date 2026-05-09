4bc6c105b48679a3563fa092b71f0b25d95655ac
MATSUMOTO Keisuke <matukotoo@gmail.com>
2026-05-09 13:59:35 +0900

    refactor(p703): ディレクトリ名の変更とパッケージ宣言の追加
    ディレクトリを 703 から p703 に移動し、各ファイルに package 宣言を追加しました。
    数字のみのディレクトリ名はjdtlsでビルドエラーになるため

--- leetcode/code/p703/KthLargest.java
+++ leetcode/code/p703/KthLargest.java
@@ -1,3 +1,5 @@
+package p703;
+
 // 703. Kth Largest
 
 import java.util.PriorityQueue;
--- leetcode/code/p703/KthLargestTest.java
+++ leetcode/code/p703/KthLargestTest.java
@@ -1,3 +1,5 @@
+package p703;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
